2e5959d21b6a12683bcbbee94ededbccf29198e8
DhivyaSathish18 <dhivyasangayar@gmail.com>
2026-05-08 19:43:45 +0530

    Initial frontend project commit

---
+++ src/app/components/url-list/url-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-url-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './url-list.component.html',
+  styleUrl: './url-list.component.css'
+})
+export class UrlListComponent {
+
+}
