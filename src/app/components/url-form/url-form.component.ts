3e6bbba83d2051e36c8fb03dea7324798675d086
DhivyaSathish18 <dhivyasangayar@gmail.com>
2026-05-09 13:59:22 +0530

    tiny url ui implementation - design

--- src/app/components/url-form/url-form.component.ts
+++ src/app/components/url-form/url-form.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UrlListComponent } from '../url-list/url-list.component';
 
 @Component({
   selector: 'app-url-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, UrlListComponent, ReactiveFormsModule],
   templateUrl: './url-form.component.html',
   styleUrl: './url-form.component.css'
 })
 export class UrlFormComponent {
 
+  tinyUrlForm: FormGroup;
+  isError: boolean = false;
+  shortUrlGenerated: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.tinyUrlForm = this.fb.group({
+      originalUrl: ['', [Validators.required, Validators.pattern('https?://.+')]],
+      isPrivate: [false]
+    })
+  }
+  OnSubmit() {
+    if (this.tinyUrlForm.valid) {
+      this.isError = false;
+      console.log(this.tinyUrlForm.value);
+      this.shortUrlGenerated = true;
+    } else {
+      this.isError = true;
+    }
+  }
 }
--- src/app/services/url.service.ts
+++ src/app/services/url.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,21 @@ import { Injectable } from '@angular/core';
 })
 export class UrlService {
 
-  constructor() { }
+  api = "https://localhost:5001";
+  constructor(private http: HttpClient) { }
+
+  create(data:any){
+    return this.http.post(`${this.api}/api/shorten`, data);
+  }
+
+  getUrls(){
+    return this.http.get(`${this.api}/api/urls`);
+  }
+  
+  delete(id: number){
+    return this.http.delete(`${this.api}/${id}`);
+  }
+  search(query: string){
+    return this.http.get(`${this.api}/search?query=${query}`);
+  }
 }
